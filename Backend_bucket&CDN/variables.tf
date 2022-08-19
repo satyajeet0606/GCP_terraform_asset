b8414249b314a2ec9a5eea925a255aa31f4b6990
satyajeet0606 <92969281+satyajeet0606@users.noreply.github.com>
2022-08-19 21:58:12 +0530

    Create variables.tf

---
+++ Backend_bucket&CDN/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    type = string
+    default = "my-trfm"
+  
+}
+
+variable "region" {
+    type = string
+    default = "europe-west1"
+  
+}
+
+variable "zone" {
+    type = string
+    default = " europe-west1-a"
+  
+}
+
+variable "location" {
+    type = string
+    default = "EU"
+  
+}
