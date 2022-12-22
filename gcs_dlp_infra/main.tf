6eee624f93fba51f50a93abcf991c30b786b41bd
satyajeet0606 <92969281+satyajeet0606@users.noreply.github.com>
2022-12-22 14:25:10 +0530

    Update main.tf

--- gcs_dlp_infra/main.tf
+++ gcs_dlp_infra/main.tf
@@ -4,14 +4,11 @@ module "gcs-infra" {
   dlp_role                 = var.dlp_role
   dlp_rolesList            = var.dlp_rolesList
   staging_bucket           = var.staging_bucket
-  non_sensitive_bucket     = var.non_sensitive_bucket
   sensitive_bucket         = var.sensitive_bucket
-  fucntion_bucket          = var.fucntion_bucket
   location                 = var.location
   force_destroy            = var.force_destroy
   storage_class            = var.storage_class
   public_access_prevention = var.public_access_prevention
-  zip_function             = var.zip_function
   pubsub_topic             = var.pubsub_topic
   pubsub_subscription      = var.pubsub_subscription
   
