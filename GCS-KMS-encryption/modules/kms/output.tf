5e37b4f88302cb1afb36c2a70d10626cbad74a30
satyajeet0606 <92969281+satyajeet0606@users.noreply.github.com>
2022-12-22 16:35:27 +0530

    Add files via upload

--- GCS-KMS-encryption/modules/kms/output.tf
+++ GCS-KMS-encryption/modules/kms/output.tf
@@ -1,18 +1,47 @@
 output "keyring" {
-    value = module.kms_key.keyring
-  
+  description = "Self link of the keyring."
+  value       = google_kms_key_ring.key_ring.id
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keyring_resource" {
-    value = module.kms_key.keyring_resource
-  
+  description = "Keyring resource."
+  value       = google_kms_key_ring.key_ring
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keys" {
-    value = module.kms_key.keys
-  
+  description = "Map of key name => key self link."
+  value       = local.keys_by_name
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
 
 output "keyring_name" {
-    value = module.kms_key.keyring_name  
+  description = "Name of the keyring."
+  value       = google_kms_key_ring.key_ring.name
+
+  # The grants are important to the key be ready to use.
+  depends_on = [
+    google_kms_crypto_key_iam_binding.owners,
+    google_kms_crypto_key_iam_binding.decrypters,
+    google_kms_crypto_key_iam_binding.encrypters,
+  ]
 }
